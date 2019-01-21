71d80c475dc452db19bba7c82a42dcd41d61c56b
Sonal Sharma <sonalshrm22@gmail.com>
2019-01-21 20:15:12 +0530

    minor

--- Investor/DB/browse.php
+++ Investor/DB/browse.php
@@ -27,14 +27,16 @@
     <?php require "../include/nav/nav.php"?>
   <div class="main">
       <div class="name">
+          <h3>BROWSE BY STARTUP NAME</h3>
           <div class="image">
-            <a href="browsebyname.php"><img src="../img/Name.png"></a>
+            <a href="browsebyname.php" title="Browse by name"><img src="../img/Name.png"></a>
           </div>
           <br>
       </div>
       <div class="ind">
+          <h3>BROWSE BY INDUSTRY TYPE</h3>
           <div class="image">
-            <a href="browsebyindustry.php"><img src="../img/IndCluster.png"></a>
+            <a href="browsebyindustry.php" title="Browse by industry"><img src="../img/IndCluster.png"></a>
           </div>
           <br>
       </div>
