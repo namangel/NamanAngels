c8b86a5e82b4792fd9634e6184421334920e7f0e
Aayush Singh <37466552+aayooush@users.noreply.github.com>
2019-01-23 19:09:06 +0530

    PitchUpload added  PDF

--- StartUp/DB/index.php
+++ StartUp/DB/index.php
@@ -34,7 +34,7 @@
 	$q = "SELECT * FROM st_uploads WHERE StpID = '$id';";
 	$results = mysqli_query($db, $q);
 	$row = mysqli_fetch_assoc($results);
-	$PitchName = $row['PitchName'];
+	$PitchName = '/NamanAngels/Uploads/'.$row['PitchName'];
 	$PitchExt = $row['PitchExt'];
 	$Logo = $row['Logo'];
     $Backimg = $row['BackImg'];
@@ -213,11 +213,11 @@
 		if (isset($name)){
 			$path= '../../Uploads/';
 			if (!empty($name)){
-				if (($fileextension !== "mp4") && ($fileextension !== "ogg") && ($fileextension !== "webm")){
+				if (($fileextension !== "mp4") && ($fileextension !== "ogg") && ($fileextension !== "webm") && ($fileextension !== "pdf")){
 					$success=0;
-					echo '<script>alert("The file extension must be .mp4, .ogg, or .webm in order to be uploaded")</script>';
+					echo '<script>alert("The file extension must be .mp4, .ogg, or .webm or .pdf in order to be uploaded")</script>';
 				}
-				else if (($fileextension == "mp4") || ($fileextension == "ogg") || ($fileextension == "webm")){
+				else if (($fileextension == "mp4") || ($fileextension == "ogg") || ($fileextension == "webm") || ($fileextension == "pdf")){
 					$success=1;
 					if (copy($tmp_name, $path.basename($_FILES['pitchvid']['name']))) {
 						echo '<script> alert("Uploaded!")</script>';
@@ -790,6 +790,14 @@
 									echo '<input type="submit" name="pitchsub" value="Upload">';
 									// echo '<div float=right><a href="#"><i class="fa fa-question-circle-o"></i>&nbsp;Need help</a></div>';
 								echo '</form>';
+							}
+							elseif ($PitchExt == 'pdf'){
+								echo '<form class="pitch" action="index.php" method="post" enctype="multipart/form-data">';
+									echo '<input type="file" name="pitchvid">';
+									echo '<input type="submit" name="pitchsub" value="Upload">';
+									// echo '<div float=right align=right><a href="#"><i class="fa fa-question-circle-o"></i>&nbsp;Need help</a></div>';
+								echo '</form>';
+								echo "<iframe src='$PitchName' style='width: 100%; height:500px'></iframe>";
 							}
 							else{
 								$videos_field=$PitchName;
