f9c39760fc87a868bcd3417f065154db6256ddad
akshi-saxena <38050818+akshi-saxena@users.noreply.github.com>
2019-01-29 20:59:49 +0530

    naman team in DB

--- include/about/team.php
+++ include/about/team.php
@@ -1,3 +1,6 @@
+<?php
+    require '../../server.php';
+?>
 <html>
     <head>
         <title>Team NAMAN</title>
@@ -9,122 +12,23 @@
       include "../header/sign.php"
       ?>
       
-        <font class="adv"><center>Our Chief Advisory</center></font>
-        <div class="advisory">
-            <div class="member">
-                <center><img src="../img/team/shweta-shalini.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Shweta Shalini</font><br>
-                <p class="designation">Official Spokesperson - Bhartiya Janta Party | Chief Evangelist -
-                The Billennium Divas Thought Leader & Speaker</p></center>
-            </div>
-            <div class="member">
-                <center><img src="../img/team/miten-mehta.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Miten Mehta</font><br>
-                <p class="designation">Co-Founder of Spinta Global Accelerato</p></center>
-            </div>
-            <div class="member">
-                <center><img src="../img/team/sandeep-sehgal.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Sandeep Sehgal</font><br>
-                <p class="designation">CEO and Co-Founder of Global ScaleUp | HQ in Singapore</p></center>
-            </div>
-            <div class="member">
-                <center><img src="../img/team/nilesh-gandhi.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Nilesh Gandhi</font><br>
-                <p class="designation">Managing Director at Unid Finance Consultancy Pvt. Ltd.</p></center>
-            </div>
-        </div>
-        <font class="adv"><center>Our Team</center></font>
-        <div class="advisory">
-            <div class="member">
-                <center><img src="../img/team/tapaswi-patel.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Tapaswi Patel</font><br>
-                <p class="designation">Serial Entrepreneur Startup Investor Founder: Naman Angels India Foundation,
-                ZoomStart India</p></center>
-            </div>
-            <div class="member">
-                <center><img src="../img/team/dinesh-israni.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Dinesh Israni</font><br>
-                <p class="designation">Co-Founder | CEO <br>TGV</p></center>
-            </div>
-            <div class="member">
-                <center><img src="../img/team/bhavesh-kothari.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Bhavesh Kothari</font><br>
-                <p class="designation">Co-Founder | CBO <br> TGV</p></center>
-            </div>
-            <div class="member">
-                <center><img src="../img/team/ankit-buti.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Ankit Buti</font><br>
-                <p class="designation">Entrepreneur in Residence with NAMAN Angels India Foundation | Founder & CEO at StartupEd</p></center>
-            </div>
-            <div class="member">
-                <center><img src="../img/team/pratik-lalani.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Pratik Lalani</font><br>
-                <p class="designation">Principal Evangelist</p></center>
-            </div>
-            <div class="member">
-                <center><img src="../img/team/purvang-joshi.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Purvang Joshi</font><br>
-                <p class="designation">Principal Evangelist</p></center>
-                </div>
-            <div class="member">
-                <center><img src="../img/team/deep-patel.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Deep Patel</font><br>
-                <p class="designation">Principal Evangelist</p></center>
-            </div>
-            <div class="member">
-                <center><img src="../img/team/sonali-shah.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Sonali Shah</font><br>
-                <p class="designation">Design & Marketing Support</p></center>
-            </div>
-            <div class="member">
-                <center><img src="../img/team/bharti-keswani.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Bharti Keswani</font><br>
-                <p class="designation">Financial Analyst</p></center>
-            </div>
-            <div class="member">
-                <center><img src="../img/team/harsha-therani.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Harsha Therani</font><br>
-                <p class="designation">Financial Analyst</p></center>
-            </div>
-            <div class="member">
-                <center><img src="../img/team/sunny-tiwari.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Sunny Tiwari</font><br>
-                <p class="designation">Financial Analyst</p></center>
-            </div>
-            <div class="member">
-                <center><img src="../img/team/yash-thakkar.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Yash Thakkar</font><br>
-                <p class="designation">Investment Fund Manager</p></center>
-            </div>
-            <div class="member">
-                <center><img src="../img/team/nikita-tilak.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Nikita Tilak</font><br>
-                <p class="designation">Investment Fund Manager</p></center>
-            </div>
-            <div class="member">
-                <center><img src="../img/team/vidisha.png">
-                <a class="linkedin" ahref="#">LINKEDIN</a><br><br>
-                <font class="name">Vidisha Dholkhedia</font><br>
-                <p class="designation">Digital Marketing</p></center>
-            </div>
-        </div>
+        <font class="adv"><center>Our Chief Advisory & Team</center></font>
+        <?php
+        $qu = "SELECT * FROM namanteam";
+        $results = mysqli_query($db, $qu);
+        echo '<div class="advisory">';
+        while($row = mysqli_fetch_assoc($results))
+        {
+                echo '<div class="member">
+                        <center>
+                        <img src="data:image/png;base64,'.base64_encode($row['image']).'"/>
+                        <a class="linkedin" ahref="#">'.$row['member_link'].'</a><br><br>
+                        <font class="name">'.$row['member_name'].'</font><br>
+                        <p class="designation">'.$row['description'].'</p></center>
+                    </div>';
+        }
+        echo '</div>';
+        ?>
         <?php
         include "../footer/footer.php"
         ?>
