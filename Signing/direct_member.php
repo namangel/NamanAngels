d17ccfcdd70036d976ccb4f6cc740af01f9e4518
akshi-saxena <38050818+akshi-saxena@users.noreply.github.com>
2019-02-23 19:58:00 +0530

    minor

--- Signing/direct_member.php
+++ Signing/direct_member.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title>Investor Membership</title>
-  <link rel="stylesheet" type="text/css" href="css/register.css">
+  <link rel="stylesheet" type="text/css" href="css/membership.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <script>
@@ -66,119 +66,20 @@ if (phone.length != 10) {
       <label>Phone</label>
       <input type="number" name="phone" required>
     </div>
-
+    <div class="input-group">
+      <textarea rows="10" name="query" placeholder="Enter queries about membership here.."></textarea>
+    </div>  
   
-    
-  
-<div class="contbot">
-      <div class="input-group">
-        <button type="submit" onclick="validateForm()"class="btn" name="dir_mem" style="background-color: #0e3c58;">Request Membership</button>
-      </div>
-      <p style="font-size:15px">
-          Already a member? <a href="login_inv.php">Sign in</a>
-      </p>
-</div>
+    <div class="contbot">
+          <div class="input-group">
+            <button type="submit" onclick="validateForm()"class="btn" name="dir_mem" style="background-color: #0e3c58;">Request Membership</button>
+          </div>
+          <p style="font-size:15px">
+              Already a member? <a href="login_inv.php">Sign in</a>
+          </p>
+    </div>
   </form>
 </div>
 <?php require "../include/footer/footer.php"?>
 </body>
 </html>
-<style>
- /* body {
-  background: white;
-  margin: 0px;
-  padding: 0px;
-}
-
-.header {
-  width: 30%;
-  margin: 10px auto 0px;
-  text-align: center;
-  padding: 10px;
-}
-.content{
-  width: 90%;
-  margin: 0px auto;
-  padding: 20px;
-  border: 1px solid #B0C4DE;
-  background: white;
-  text-align: center;
-}
-.input-group{
-  position: relative;
-  display: inline-block;
-  width:48%;
-}
-
-.input-group label, .input-group input, .input-group select{
-  margin: 10px 0px 10px 0px;
-  display: inline-block;
-  width: 40%;
-  padding: 10px;
-  font-size: 16px;
-  text-align: left;
-}
-
-
-.input-group .tooltiptext {
-  visibility: hidden;
-  width: 275px;
-  background-color: #555;
-  color: #fff;
-  text-align: center;
-  border-radius: 6px;
-  padding: 5px 0;
-  position: absolute;
-  z-index: 1;
-  bottom: 125%;
-  left: 60%;
-  margin-left: -60px;
-  opacity: 0;
-  transition: opacity 0.3s;
-  }
-
-  .input-group .tooltiptext::after {
-  content: "";
-  position: absolute;
-  top: 100%;
-  left: 50%;
-  margin-left: -5px;
-  border-width: 5px;
-  border-style: solid;
-  border-color: #555 transparent transparent transparent;
-  }
-
-  .input-group:hover .tooltiptext {
-  visibility: visible;
-  opacity: 1;
-  }
-.contbot{
-    display: block;
-    text-align: right;
-    margin: 50px;
-}
-.contbot .btn {
-  padding: 10px;
-  font-size: 15px;
-  color: white;
-  border: none;
-  border-radius: 5px;
-}
-#individual, #insitution{
-  display: none;
-}
-.type{
-    display: inline-block;
-    width:40%;
-}
-
-.type label, .type input, .type select{
-  margin: 0px 0px 10px 0px;
-  display: inline-block;
-  width: 40%;
-  padding: 10px;
-  font-size: 16px;
-  text-align: left;
-}
-
-  </style>
