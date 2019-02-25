6e24ac96ea66aab8a2cf79710c6b5285e9b2db91
readthroughmyglasses <37587359+readthroughmyglasses@users.noreply.github.com>
2019-02-25 20:16:00 +0530

    updated membership

--- admin/estmem.php
+++ admin/estmem.php
@@ -22,20 +22,22 @@
 
 <style>
     .card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  margin: auto;
-  width: 80%;
-  text-align: center;
-  font-family: arial;
-  background-color:white;
-  color:black;
+      font-family: "Segoe UI", "Roboto";
+   margin-left: 350px;
+   border-radius: 5px;
+   overflow: hidden;
+   width: 450px;
+   padding: 15px 25px;
+   box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
+   background-color:white;
+  
 }
 .teamform{
         background-color: #ccc4d4;
         float: center;
         border: 2px solid black;
         margin-top: 10px;
-        margin-left:400px;
+        margin-left:350px;
         padding: 20px;
         width: 60%;
         color:black;
@@ -65,21 +67,24 @@
                         <input type="submit" name="get_details" value="getdetails">
                     </form>
             </div>
-
+<br>
             <?php 
-                if (isset($_POST["getdetails"]))
+                if (isset($_POST["get_details"]))
                 {
                   
                   $InvID = mysqli_real_escape_string($db, $_POST['inv_id']);
                   $check_query = "SELECT * FROM aview WHERE InvID='$InvID' ;";
-                     echo '<div class="card">';
+                  $result = mysqli_query($db, $check_query);
+                  $row=mysqli_fetch_assoc($result);
+                     echo '<div class="card"><br>';
                      
-                            echo '<h1>'.$row['CName'].'</h1>';
-                            echo '<h1>'.$row['FName'].'</h1>';
-                            echo '<h1>'.$row['LName'].'</h1>';
-                          echo '<p>'.$row['Type'].'</p>';
+                            echo '<h3 style="color:#ff8533";>'.$row['CName'].'</h3>';
+                            echo '<h3><i class="fa fa-id-card-o" aria-hidden="true"></i>&nbsp;'.$row['FName'].'</h3>';
+                            echo '<h3><i class="fa fa-user" aria-hidden="true"></i>&nbsp;'.$row['LName'].'</h3>';
+                          echo '<h3> <i class="fa fa-circle" aria-hidden="true"></i>&nbsp;'.$row['Type'].'</h3>';
+                         echo '<a href="#?s='.$row['InvID'].'" target=_blank style="text-decoration:none;">Accept Membership</a>';
                        echo '</div>';
-                 header('location:estmem.php');
+                
               }         
             
             
