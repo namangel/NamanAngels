4feca31e327fdc7e6755afb876ba767d39c5cf87
Nitish <angelnaman07@gmail.com>
2019-02-26 14:46:05 +0530

    Membership

--- admin/estmem.php
+++ admin/estmem.php
@@ -1,21 +1,5 @@
 <?php require('../server.php'); ?>
 <?php require "sidebar.php"; ?>
-<?php
-// if (isset($_POST["getdetails"]))
-//   {
-    
-//     $InvID = mysqli_real_escape_string($db, $_POST['inv_id']);
-//     $check_query = "SELECT * FROM aview WHERE InvID='$InvID' ;";
-//        echo '<div class="card">';
-       
-//               echo '<h1>'.$row['CName'].'</h1>';
-//               echo '<h1>'.$row['FName'].'</h1>';
-//               echo '<h1>'.$row['LName'].'</h1>';
-//             echo '<p>'.$row['Type'].'</p>';
-//          echo '</div>';
-//    header('location:estmem.php');
-// }    
-?>
 
 <html>
 <head>
@@ -31,7 +15,7 @@
    padding: 15px 25px;
    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
    background-color:white;
-  
+
 }
   .teamform{
     background-color: #ccc4d4;
@@ -49,7 +33,7 @@
     border: none;
     color: white;
     cursor: pointer;
-    
+
   }
   input[type=submit]{
         background-color:  #ff8533;
@@ -57,11 +41,17 @@
         color: white;
         padding: 10px;
     }
+    .btn{
+      background-color:  #ff8533;
+      border: none;
+      color: white;
+      padding: 10px;
+    }
   </style>
   </head>
 <body>
 <div class="teamform">
-                   
+
     <form method="POST" action="estmem.php">
         <label>Inv Id:</label><br>
         <input type="text" name="inv_id"><br><br>
@@ -69,29 +59,51 @@
     </form>
   </div>
 <br>
-  <?php 
+  <?php
       if (isset($_POST["get_details"]))
       {
-        
-        $InvID = mysqli_real_escape_string($db, $_POST['inv_id']);
-        $check_query = "SELECT * FROM aview WHERE InvID='$InvID' ;";
-        $result = mysqli_query($db, $check_query);
-        $row=mysqli_fetch_assoc($result);
+        if (empty($_POST['inv_id'])){
+          header('location: estmem.php');
+        }
+        else{
+          $InvID = mysqli_real_escape_string($db, $_POST['inv_id']);
+          $check_query = "SELECT * FROM aview WHERE InvID='$InvID' ;";
+          $result = mysqli_query($db, $check_query);
+          $num = mysqli_num_rows($result);
+          if($num ==0){
             echo '<div class="card"><br>';
-            
-            echo '<h3 style="color:#ff8533";>'.$row['CName'].'</h3>';
-            echo '<h3><i class="fa fa-id-card-o" aria-hidden="true"></i>&nbsp;'.$row['FName'].'</h3>';
-            echo '<h3><i class="fa fa-user" aria-hidden="true"></i>&nbsp;'.$row['LName'].'</h3>';
-            echo '<h3> <i class="fa fa-circle" aria-hidden="true"></i>&nbsp;'.$row['Type'].'</h3>';
-            echo '<a href="#?s='.$row['InvID'].'" target=_blank style="text-decoration:none;">Accept Membership</a>';
+
+            echo '<h3 style="color:#ff8533";>Investor Does Not Exist</h3>';
+            echo '<h3>Investor Id Incorrect</h3>';
             echo '</div>';
-      
-    }         
-  
-  
+          }
+          else{
+            $row=mysqli_fetch_assoc($result);
+            if(empty($row['MemID'])){
+                echo '<div class="card"><br>';
+
+                echo '<h3 style="color:#ff8533";>'.$row['CName'].'</h3>';
+                echo '<h3><i class="fa fa-id-card-o" aria-hidden="true"></i>&nbsp;'.$row['FName'].'</h3>';
+                echo '<h3><i class="fa fa-user" aria-hidden="true"></i>&nbsp;'.$row['LName'].'</h3>';
+                echo '<h3> <i class="fa fa-circle" aria-hidden="true"></i>&nbsp;'.$row['Type'].'</h3><br>';
+                echo '<a href="estmembership.php?inid='.$row['InvID'].'"target=_blank><button class="btn">Accept Membership</button></a>';
+                // echo '<form method="POST" action="estmem.php">'.'<input type="submit" name="accept" value="Accept Membership">'.'</form>';
+                echo '</div>';
+                // header('location: estmem.php');
+                // $memid = strtoupper('MEM'.substr(sha1($invID, FALSE), 0, 8));
+              }
+              else{
+                echo '<div class="card"><br>';
+
+                echo '<h3 style="color:#ff8533";>Membership Exists</h3>';
+                echo '<h3>Investor is Already a Member</h3>';
+                echo '</div>';
+              }
+            }
+          }
+    }
   ?>
 
 </body>
 
 </html>
-
